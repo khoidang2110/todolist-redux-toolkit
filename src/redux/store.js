b1b9ec58ad91f0a9b515b380dd0dc712d6223eea
khoidang2110 <khoidang2110@gmail.com>
2023-07-20 22:59:39 +0700

    complete

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+// import {createStore} from 'redux';
+// import rootReducer from './reducer';
+// import {composeWithDevTools} from 'redux-devtools-extension';
+
+// const composeEnhancers = composeWithDevTools();
+// const store = createStore(rootReducer,composeEnhancers);
+
+// export default store;
+
+import {configureStore} from '@reduxjs/toolkit';
+import filtersSlice from '../components/Filters/filtersSlice';
+import todosSlice from '../components/TodoList/todosSlice';
+
+const store = configureStore({
+    reducer: {
+        filters:filtersSlice.reducer,
+        todoList:todosSlice.reducer,
+    },
+});
+
+export default store;
